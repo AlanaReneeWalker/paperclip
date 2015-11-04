d1e438891e775096d8165710bc96868437614224
Alana Walker <pwd>
2015-11-04 15:44:47 -0500

    paperclip assignment

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  
+  def index
+    @post = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+    
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post   
+    else
+      render 'new'
+    end
+  end
+
+  def dogs
+    @posts = Post.with_posts_about_dogs
+  end
+
+  def edit
+
+    @post = Post.find(params[:id])
+  end
+
+  def update
+
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
+def post_params
+  params.require(:post).permit(:title, :body)
+end
+
+end
+
